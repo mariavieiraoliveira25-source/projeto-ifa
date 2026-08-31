e72e64fff9bf534a713eeef943ecba656f6ff3ad
mariavieiraoliveira25-source <maria.vieira.oliveira25@escola.pr.gov.br>
2026-08-31 10:41:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -256,8 +256,8 @@ function trocarImagem(escolha) {
     switch(escolha){
         case "1":
           urlNova = 'café.png';
-          textoprincipal.innerText="Como explicar o Sol?";
-          texto.innerText="O Sol é uma estrela gigante feita de gás quente que fica no centro do nosso Sistema Solar, a 150 milhões de quilômetros da Terra. Ele dá luz e calor ao nosso planeta, controla a gravidade que segura os planetas ao seu redor e é essencial para a existência de toda a vida.";
+          textoprincipal.innerText="fases da lua";
+          texto.innerText="";
           descrevefigura.innerText="Imagem do Sol na cor amarela ";
           break;
         case "2":
